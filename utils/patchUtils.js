2224cb7499ce505dfd804465ac9775f7eae199b6
Jose Munoz <jdmg93@gmail.com>
2021-05-24 14:46:05 -0700

    fixes the patch reversal utility

--- utils/patchUtils.js
+++ utils/patchUtils.js
@@ -1,42 +1,41 @@
-import { produce, produceWithPatches, applyPatches } from 'immer'
+import { produce, produceWithPatches, applyPatches, original } from "immer";
 
-const noop = () => void 0
+const noop = () => void 0;
 
 export const produceWithPatch = (state, id, mutations = noop) => {
-  if(!id) {
-    throw new Error('must include Id for undoable actions')
+  if (!id) {
+    throw new Error("must include Id for undoable actions");
   }
 
   const [nextState, patches, inversePatches] = produceWithPatches(
     state,
-    draft => {
-      mutations(draft)
-    },
-  )
-  
-  const buffer = produce(nextState, draft => {
+    (draft) => {
+      mutations(draft);
+    }
+  );
+
+  return produce(nextState, (draft) => {
     draft.undoStack[id] = {
       patches,
-      inversePatches
-    }
-  })
-
-  return buffer
-}
+      inversePatches,
+    };
+  });
+};
 
 export const produceWithReversal = (state, id, mutations = noop) => {
-  if(!id) {
-    throw new Error('must include Id for reversal actions')
-  }
-  
-  if (!state.undoStack[id]) {
-    throw new Error('Invalid patch Id')
+  if (!id) {
+    throw new Error("must include Id for reversal actions");
   }
 
-  const reversal = state.undoStack[id].inversePatches
-  
-  return produce(applyPatches(state, reversal), draft => {
-    mutations(draft)
-    delete draft.undoStack[id] 
-  })
-}
+  if (!state.undoStack[id]) {
+    throw new Error("Invalid patch Id");
+  }
+
+  const reversal = state.undoStack[id].inversePatches;
+
+  return produce(state, (draft) => {
+    applyPatches(draft, reversal)
+    mutations(draft);
+    delete draft.undoStack[id];
+  });
+};
